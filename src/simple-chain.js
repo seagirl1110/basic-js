1492223d072fb87eef6f9bbb363de1813079aa71
Marina Elizarova <seagirl1110@gmail.com>
2022-03-15 19:03:34 +0300

    feat: finish simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  'arr': [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    let chainLink = `( ${value} )`
+    if (value === undefined) {
+      chainLink = `( )`;
+    }
+    this.arr.push(chainLink);
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof(position) !== 'number' || position <= 0 || position > this.getLength()) {
+      this.arr = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.arr.splice(position - 1, 1);
+      return chainMaker;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return chainMaker;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const finish = this.arr.join('~~');
+    this.arr = [];
+    return finish;
   }
 };
 
