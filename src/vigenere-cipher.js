8d8130fb84ca63e844ba7faae38704d34358e00b
Marina Elizarova <seagirl1110@gmail.com>
2022-04-25 17:48:22 +0300

    feat: finish task Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    while (key.length < message.length) {
+      key += key;
+    }
+    let text = message.toUpperCase();
+    let keyArr = key.toUpperCase().split('');
+    let arr = [];
+    let codeA = 'A'.charCodeAt(0)
+    for (let i = 0; i < text.length; i += 1) {
+      if (text.charCodeAt(i) > 64 && text.charCodeAt(i) < 91) {
+        const current = text.charCodeAt(i) - codeA;
+        const shift = keyArr[0].charCodeAt(0) - codeA;
+        keyArr.shift();
+        const el = String.fromCharCode(codeA + (current + shift) % 26);
+        arr.push(el);
+      } else {
+        arr.push(text[i]);
+      }
+    }
+
+    if (this.type === false) {
+      return arr.reverse().join('');
+    }
+    return arr.join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    while (key.length < message.length) {
+      key += key;
+    }
+    let text = message.toUpperCase();
+    let keyArr = key.toUpperCase().split('');
+    let arr = [];
+    let codeA = 'A'.charCodeAt(0)
+    for (let i = 0; i < text.length; i += 1) {
+      if (text.charCodeAt(i) > 64 && text.charCodeAt(i) < 91) {
+        const current = text.charCodeAt(i) - codeA;
+        const shift = keyArr[0].charCodeAt(0) - codeA;
+        keyArr.shift();
+        const el = String.fromCharCode(codeA + (current - shift + 26) % 26);
+        arr.push(el);
+      } else {
+        arr.push(text[i]);
+      }
+    }
+
+    if (this.type === false) {
+      return arr.reverse().join('');
+    }
+    return arr.join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
